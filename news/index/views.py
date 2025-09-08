c4b2925a4fd04e98dd1e4b670bd7b6b934d25c31
Пользователь <nikolas.dzyuba@gmail.com>
2025-09-08 23:49:16 +0500

    Сделано много

--- news/index/views.py
+++ news/index/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from unicodedata import category
+
 from .models import Category, News
 
 
@@ -12,3 +14,23 @@ def home_page (request):
         'news': news
     }
     return render(request, 'home.html', context)
+
+def news_page (request, pk):
+    news = News.objects.get(id=pk)
+    categories = Category.objects.all()
+    context = {
+        'categories': categories,
+        'news': news
+    }
+    return render(request, 'news_page.html', context)
+
+def category_page (request, pk):
+    categories = Category.objects.get(id=pk)
+    news = News.objects.filter(news_category=categories)
+    all_categories = Category.objects.all()
+    context = {
+        'categories': categories,
+        'news': news,
+        'all_categories': all_categories
+    }
+    return render(request, 'news_on_category.html', context)
